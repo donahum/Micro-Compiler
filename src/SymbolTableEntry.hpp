d91d0b9e00ac0c2259db2bf7d2664be8394dba58
Michael Donahue <donahum@purdue.edu>
2018-10-15 13:31:24 -0400

    updated symbol table

---
+++ src/SymbolTableEntry.hpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+class SymbolTableEntry
+{
+	public:
+		SymbolTableEntry();
+
+		SymbolTableEntry(SymbolTableEntry * newEntry);
+
+		SymbolTableEntry(std::string nm, std::string tp, std::string val);
+		SymbolTableEntry(std::string nm, std::string tp);
+
+		SymbolTableEntry(std::string * nm, std::string * tp, std::string * val);
+		SymbolTableEntry(std::string * nm, std::string * tp);
+
+		void printSTE();
+
+		std::string getName();
+		std::string getType();
+		std::string getValue();
+
+		void setName(std::string x);
+		void setType(std::string x);
+		void setValue(std::string x);
+
+		std::string name;
+		std::string type;
+		std::string value;
+
+	private:
+		
+		//friend class SymbolTable;
+};
