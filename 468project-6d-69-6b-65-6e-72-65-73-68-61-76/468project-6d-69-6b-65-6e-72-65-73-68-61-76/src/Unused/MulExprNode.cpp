b43b53233032aa3b1d9782399870353669cdc969
Michael Donahue <donahum@purdue.edu>
2018-10-17 23:52:19 -0400

    it feels good to be clean

--- 468project-6d-69-6b-65-6e-72-65-73-68-61-76/468project-6d-69-6b-65-6e-72-65-73-68-61-76/src/Unused/MulExprNode.cpp
+++
@@ -1,9 +0,0 @@
-#include "MulExprNode.hpp"
-
-MulExprNode::MulExprNode(std::string inputOp, ASTNodeType type) : ASTNode::ASTNode(type){
-	mul_op = inputOp;
-	//Type = type;
-}
-std::string MulExprNode::getOp(){
-	return mul_op;
-}
--- 468project-6d-69-6b-65-6e-72-65-73-68-61-76/468project-6d-69-6b-65-6e-72-65-73-68-61-76/src/Unused/VarRefNode.cpp
+++
@@ -1,9 +0,0 @@
-#include "VarRefNode.hpp"
-
-VarRefNode::VarRefNode(std::string idSrc, ASTNodeType type) : ASTNode::ASTNode(type){
-	ident = idSrc;
-	//Type = type;
-}
-std::string VarRefNode::getIdent(){
-	return ident;
-}
